0b18828224193584157ab45a93d5f5a39b2488ce
vanessavvp <vanessavillalbap@gmail.com>
2022-05-20 20:27:27 +0100

    Adding server and other advances

---
+++ Server/server.py
@@ -0,0 +1,133 @@
+import struct
+import time
+import zmq
+import PIL
+import tensorflow as tf
+from keras.models import load_model
+import numpy as np
+import array 
+import cv2
+
+"""
+    Tiny NeRF logic
+"""  
+def posenc(x):
+  rets = [x]
+  for i in range(L_embed):
+    for fn in [tf.sin, tf.cos]:
+      rets.append(fn(2.**i * x))
+  return tf.concat(rets, -1)
+
+L_embed = 6
+embed_fn = posenc
+N_samples = 64
+
+HEIGHT = 100
+WIDTH = 100
+focal = 145
+  
+trans_t = lambda t : tf.convert_to_tensor([
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,t],
+    [0,0,0,1],
+], dtype=tf.float32)
+
+rot_phi = lambda phi : tf.convert_to_tensor([
+    [1,0,0,0],
+    [0,tf.cos(phi),-tf.sin(phi),0],
+    [0,tf.sin(phi), tf.cos(phi),0],
+    [0,0,0,1],
+], dtype=tf.float32)
+
+rot_theta = lambda th : tf.convert_to_tensor([
+    [tf.cos(th),0,-tf.sin(th),0],
+    [0,1,0,0],
+    [tf.sin(th),0, tf.cos(th),0],
+    [0,0,0,1],
+], dtype=tf.float32)
+
+def get_rays(H, W, focal, c2w):
+    i, j = tf.meshgrid(tf.range(W, dtype=tf.float32), tf.range(H, dtype=tf.float32), indexing='xy')
+    dirs = tf.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -tf.ones_like(i)], -1)
+    rays_d = tf.reduce_sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)
+    rays_o = tf.broadcast_to(c2w[:3,-1], tf.shape(rays_d))
+
+    return rays_o, rays_d
+
+
+def render_rays(network_fn, rays_o, rays_d, near, far, N_samples, rand=False):
+    # rays_d = tf.cast(rays_d, tf.dtypes.float32)
+    rays_o = tf.cast(rays_o, tf.dtypes.float32)
+    #print(rays_d)
+    #print(rays_o)
+
+    def batchify(fn, chunk=1024*32):
+        return lambda inputs : tf.concat([fn(inputs[i:i+chunk]) for i in range(0, inputs.shape[0], chunk)], 0)
+    
+    # Compute 3D query points
+    z_vals = tf.linspace(near, far, N_samples) 
+    if rand:
+      z_vals += tf.random.uniform(list(rays_o.shape[:-1]) + [N_samples], dtype=tf.dtypes.float32) * (far-near)/N_samples
+    pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None]
+    
+    # Run network
+    pts_flat = tf.reshape(pts, [-1,3])
+    pts_flat = embed_fn(pts_flat)
+    raw = batchify(network_fn)(pts_flat)
+    raw = tf.reshape(raw, list(pts.shape[:-1]) + [4])
+    
+    # Compute opacities and colors
+    sigma_a = tf.nn.relu(raw[...,3])
+    rgb = tf.math.sigmoid(raw[...,:3]) 
+    
+    # Do volume rendering
+    dists = tf.concat([z_vals[..., 1:] - z_vals[..., :-1], tf.broadcast_to([1e10], z_vals[...,:1].shape)], -1) 
+    alpha = 1.-tf.exp(-sigma_a * dists)  
+    weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
+    
+    rgb_map = tf.reduce_sum(weights[...,None] * rgb, -2) 
+    depth_map = tf.reduce_sum(weights * z_vals, -1) 
+    acc_map = tf.reduce_sum(weights, -1)
+
+    return rgb_map, depth_map, acc_map
+
+def pose_spherical(theta, phi, radius):
+    c2w = trans_t(radius)
+    c2w = rot_phi(phi/180.*np.pi) @ c2w
+    c2w = rot_theta(theta/180.*np.pi) @ c2w
+    c2w = np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]]) @ c2w
+    return c2w
+
+def f(**kwargs):
+    c2w = pose_spherical(**kwargs)
+    rays_o, rays_d = get_rays(HEIGHT, WIDTH, focal, c2w[:3,:4])
+    rgb, depth, acc = render_rays(model, rays_o, rays_d, near=2., far=6., N_samples=N_samples)
+    rgb_255 = rgb * 255
+    # print(np.max(rgb_255))
+    rgb = tf.dtypes.cast(rgb_255, tf.uint8)
+    rgb_encoded_array = tf.io.encode_png(rgb)
+    # print(rgb_encoded_array)
+    
+    return rgb_encoded_array.numpy()
+    
+    
+if __name__ == "__main__":
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+    model = load_model('./tiny_nerf.h5')
+    
+    while True:
+        coordinates = socket.recv(1024)
+        coordinates = array.array('f', coordinates).tolist()
+        print("\nSpherical coordinates received: %s" % coordinates)
+
+        start = time.time()
+        img = f(**{"theta": coordinates[0], "phi": coordinates[1], "radius": coordinates[2]})
+        end = time.time()
+        print("\nThe execution time of NeRF was of: ", end - start, " seconds\nSending encoded img... ")
+        socket.send(img)
+        
+
+
